8467be496db7723310bc57524eb448291014465d
TLS-Escanor <patilprajwal8804@gmail.com>
2024-07-23 22:39:48 +0530

    conditionalOperator

---
+++ javaPrograms/conditionalOperator/Greatest1.java
@@ -0,0 +1,19 @@
+class Greatest1
+{
+	public static void main(String[] args) 
+	{
+		
+	int n1=10;
+	int n2= 20;
+	int n3= 30;
+	int n4=40;
+	
+	int greater1 = n1 > n2 ? n1 : n2;
+	int greater2 = n3 > n4 ? n3 : n4;
+	
+	System.out.println(greater1);
+		System.out.println(greater2);
+	
+
+	}
+}
