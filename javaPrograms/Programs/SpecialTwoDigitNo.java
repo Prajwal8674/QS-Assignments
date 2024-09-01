88c54ad160587c2a843f8838cff7b170a858881d
intellij <patilprajwal8804@gmail.com>
2024-09-01 14:25:22 +0530

    SpecialTwoDigitNo

--- javaPrograms/Programs/SpecialTwoDigitNo.java
+++ javaPrograms/Programs/SpecialTwoDigitNo.java
@@ -3,19 +3,30 @@ import java.util.Scanner;
 
 public class SpecialTwoDigitNo {
     public static void checkSpecial(int num) {
-
+        int sum=num;
         int rem=0;
+        int add=0;
+        int product=1;
       while(num>0){
-
           rem=num%10;
-          System.out.println(rem);
+
           num=num/10;
+          add+=rem;
+          product*=rem;
       }
+        System.out.println("THE SUM OF THE DIGITS : "+add);
+        System.out.println("THE PRODUCT OF THE DIGITS : "+product);
+
+        if(add+product==sum){
+            System.out.println("THIS IS SPECIAL TWO DIGIT NUMBER !!");
+        }else{
+            System.out.println("THIS IS NOT A SPECIAL TWO DIGIT NUMBER !!");
+        }
     }
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("ENTER THE NUMBER : ");
+    System.out.print("ENTER THE NUMBER : ");
     int num = sc.nextInt();
     checkSpecial(num);
     }
