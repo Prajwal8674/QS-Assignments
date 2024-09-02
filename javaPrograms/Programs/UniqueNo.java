b481a9e1db284eccb637cd01be87b4121b76d1f1
intellij <patilprajwal8804@gmail.com>
2024-09-02 23:08:25 +0530

    ReverseNo

--- javaPrograms/Programs/UniqueNo.java
+++ javaPrograms/Programs/UniqueNo.java
@@ -1,42 +1,43 @@
-package Programs;
-import java.util.Scanner;
+        package Programs;
+        import java.util.Scanner;
 
-public class UniqueNo {
+        public class UniqueNo {
 
-    public static void main(String[] args) {
+            public static void main(String[] args) {
 
-        int r1, r2, number, num1, num2, count = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("ENTER THE NUMBER : ");
-        number = sc.nextInt();
+                int r1, r2, number, num1, num2, count = 0;
+                Scanner sc = new Scanner(System.in);
+                System.out.print("ENTER THE NUMBER : ");
+                number = sc.nextInt();
 
-        num1 = number;
-        num2 = number;
+                num1 = number;
+                num2 = number;
 
-        while (num1 > 0)
-        {
-            r1 = num1 % 10;
-
-
-            while (num2 > 0)
-            {
-                r2 = num2 % 10;
-
-                if (r1 == r2)
+                while (num1 > 0)
                 {
-                    count++;
+                    r1 = num1 % 10;
+
+                    while (num2 > 0)
+                    {
+                        r2 = num2 % 10;
+
+                        if (r1 == r2)
+                        {
+                            count++;
+                        }
+                        num2 = num2 / 10;
+                    }
+                    num1 = num1 / 10;
                 }
-                num2 = num2 / 10;
+                if (count == 1)
+                {
+                    System.out.println("UNIQUE");
+                }
+
+
+                else
+                {
+                    System.out.println("NOT UNIQUE");
+                }
             }
-            num1 = num1 / 10;
         }
-        if (count == 1)
-        {
-            System.out.println("UNIQUE");
-        }
-        else
-        {
-            System.out.println("NOT UNIQUE");
-        }
-    }
-}
