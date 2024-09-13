9066154689fe552f38b30bbb28436b6d50caa25c
intellij <patilprajwal8804@gmail.com>
2024-09-13 20:41:58 +0530

    Commit

---
+++ javaPrograms/Programs/Factorial.java
@@ -0,0 +1,17 @@
+package Programs;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER THE NUMBER TO FIND ITS FACTORIAL : ");
+        int num=sc.nextInt();
+        int fact=1;
+
+        for(int i=1;i<=num;i++){
+        fact*=i;
+        }
+        System.out.println(fact);
+    }
+}
