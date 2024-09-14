71de9925a275c42a9c5d7ba13aea6cd4b3db968d
intellij <patilprajwal8804@gmail.com>
2024-09-15 01:03:19 +0530

    array

---
+++ Arrays/SingleDimension.java
@@ -0,0 +1,23 @@
+
+
+// datatype variable[]=new datatype[size];
+
+public class SingleDimension {
+
+    public static void main(String[] args) {
+
+        int array[] = new int[10];
+        array[0]=1;
+        array[1]=2;
+        array[2]=3;
+        array[3]=4;
+        array[4]=5;
+        array[5]=6;
+        array[6]=7;
+        array[7]=8;
+        array[8]=9;
+        array[9]=10;
+
+        System.out.println(array[9]);
+    }
+}
