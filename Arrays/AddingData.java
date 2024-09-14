f3fd69874c4c7f5662e4099503045b4c36e73cb9
intellij <patilprajwal8804@gmail.com>
2024-09-15 01:34:02 +0530

    array

---
+++ Arrays/AddingData.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AddingData {
+
+    public static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int arr[] = new int[5];
+        System.out.println("Enter The Number : ");
+        arr[0]=sc.nextInt();
+        System.out.println(arr[0]);
+
+    }
+}
