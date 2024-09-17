e642b162b1ac72cde46872f228278a19118a6d69
intellij <patilprajwal8804@gmail.com>
2024-09-17 14:59:17 +0530

    array

---
+++ Arrays/MultiDimensional.java
@@ -0,0 +1,20 @@
+public class MultiDimensional {
+    public static void main(String[] args) {
+
+        int [][] num={{1,2,3,4},{5,6,7,8}};
+
+        System.out.print(num[0][0]+"  ");
+        System.out.println(num[1][0]);
+        System.out.print(num[0][1]+"  ");
+        System.out.println(num[1][1]);
+        System.out.print(num[0][2]+"  ");
+        System.out.println(num[1][2]);
+        System.out.print(num[0][3]+"  ");
+        System.out.println(num[1][3]);
+
+
+
+
+    }
+
+}
