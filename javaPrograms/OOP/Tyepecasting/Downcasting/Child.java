915ccc27ad7f2410a52664e61733c733470e16db
intellij <patilprajwal8804@gmail.com>
2024-10-11 12:21:32 +0530

    ABSTRACTION

--- javaPrograms/OOP/Tyepecasting/Downcasting/Child.java
+++ javaPrograms/OOP/Tyepecasting/Downcasting/Child.java
@@ -4,4 +4,9 @@ public class Child extends Father{
     public void Sing(){
         System.out.println("Can Sing");
     }
+    // if I override
+
+    public void dance() {
+        System.out.println("Can Dance without song");
+    }
 }
