498c1ec704e24f5c125730abd86d734546ef40b0
intellij <patilprajwal8804@gmail.com>
2024-10-16 01:43:42 +0530

    OBJECT CLASS

--- javaPrograms/ObjectClass/Laptop/LaptopDriver.java
+++ javaPrograms/ObjectClass/Laptop/LaptopDriver.java
@@ -6,7 +6,13 @@ public class LaptopDriver {
         Laptop l1 = new Laptop("ASUS","TUF F15",72000,16,1);
         Laptop l2 = new Laptop("ASUS","TUF F15",72000,16,1);
 
+        System.out.println(l1);
+        System.out.println(l2);
+
         if(l1.equals(l2)){
+            System.out.println("There Is Duplicate Laptop in Shop");
+        }
+        if(l1==l2){
             System.out.println("There Is Duplicate Laptop in Shop");
         }
 
