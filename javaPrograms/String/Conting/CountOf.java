0b4e8ff45f289a1cead35d598b0091b61b25a2b4
intellij <patilprajwal8804@gmail.com>
2024-11-26 01:42:36 +0530

    mini project

--- javaPrograms/String/Conting/CountOf.java
+++ javaPrograms/String/Conting/CountOf.java
@@ -3,26 +3,41 @@ package String.Conting;
 public class CountOf {
     public static void main(String[] args) {
 
-        String str = "Prajwal123";
-        String up =str.toUpperCase();
-        String low= str.toLowerCase();
+        String str = "Prajwal124$#";
+        String str2=str;
+
         int upperCase=0;
         int lowerCase=0;
         int digits =0;
+        int specialChar=0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if ((int)str.charAt(i)>=128 || (int)str.charAt(i)<=47 ) {
+                specialChar++;
+            }
+            else if ((int)str.charAt(i)>=48 && (int)str.charAt(i)<=57) {
+                digits++;
+            }
+        }
+
+        String up =str.toUpperCase();
+        String low= str.toLowerCase();
+
         for (int i = 0; i < str.length(); i++) {
             if(str.charAt(i)==up.charAt(i)){
                 upperCase++;
             }
-            else if (str.charAt(i)==low.charAt(i)) {
+            if (str.charAt(i)==low.charAt(i)) {
                 lowerCase++;
             }
-            else if (str.charAt(i)<10) {
-                digits++;
-            }
         }
+
+        System.out.println("STRING : "+str);
         System.out.println("TOTAL UPPERCASE : "+upperCase);
         System.out.println("TOTAL LOWERCASE : "+lowerCase);
         System.out.println("TOTAL DIGITS : "+digits);
-        System.out.println(up);
+        System.out.println("TOTAL SPECIAL CHARACTERS : "+specialChar);
+
     }
 }
