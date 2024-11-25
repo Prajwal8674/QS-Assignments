d790b5ea84811be704784e6d86dcc6019a68701c
intellij <patilprajwal8804@gmail.com>
2024-11-25 23:27:39 +0530

    mini project

--- javaPrograms/String/CopyString/CopyString.java
+++ javaPrograms/String/CopyString/CopyString.java
@@ -1,4 +1,14 @@
 package String.CopyString;
 
 public class CopyString {
+    public static void main(String[] args) {
+
+        String s1 = "PRAJWAL";
+        String ans="";
+
+        for(int i = 0 ; i<s1.length(); i++){
+            ans+=s1.charAt(i);
+        }
+        System.out.println(ans);
+    }
 }
