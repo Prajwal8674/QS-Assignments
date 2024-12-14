6458ca2d080bcc3446cd3826ac20434bba7f6f17
intellij <patilprajwal8804@gmail.com>
2024-12-14 12:48:06 +0530

    String Problems

---
+++ javaPrograms/Collection/Arlst.java
@@ -0,0 +1,27 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class Arlst {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("hello");
+        list.add("hello1");
+        list.add("hello2");
+
+        /*
+       for (int i = 0; i < list.size(); i++) {
+            String value = list.get(i);
+            if(value.length()%2==0){
+                System.out.println(value);
+            }
+       } */
+        for(String value : list){
+            if(value.length()%2==0){
+                System.out.println(value);
+            }
+        }
+    }
+}
