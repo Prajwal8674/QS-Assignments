97d431c87ba3b91db0a5c83e73095fd5685c8ca3
intellij <patilprajwal8804@gmail.com>
2025-06-24 18:29:00 +0530

    done

---
+++ Selenium/selenium/Basic.java
@@ -0,0 +1,36 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Basic {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		String expectedUrl = "https://demowebshop.tricentis.com/";
+		ChromeDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+	
+		driver.get("https://demowebshop.tricentis.com/");
+		String actualUrl= driver.getCurrentUrl();
+		 if(expectedUrl.equals(actualUrl)) {
+			 
+			 driver.findElement(By.className("ico-register")).click();
+			 Thread.sleep(1000);
+			 driver.findElement(By.className("ico-login")).click();
+			 Thread.sleep(1000);
+
+			 driver.findElement(By.className("ico-cart")).click();
+			 Thread.sleep(1000);
+
+			 driver.findElement(By.className("ico-wishlist")).click();
+			 Thread.sleep(1000);
+
+			 driver.close();
+		 }else {
+			 System.out.println("we are not in the dws page");
+		 }
+		
+	}
+
+}
