cc9dc404147052ee05eea05eb26d43c3c812e06e
intellij <patilprajwal8804@gmail.com>
2025-08-17 23:17:43 +0530

    done

---
+++ Selenium/iFrame/FrameByIntArg.java
@@ -0,0 +1,21 @@
+package iFrame;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FrameByIntArg {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.dream11.com/");
+		Thread.sleep(1000);
+		
+		driver.switchTo().frame(0);
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("1234567890");
+		
+	}
+
+}
