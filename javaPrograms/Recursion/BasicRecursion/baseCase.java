38c0fc55e197496395e8d76a7b07dbbd4b04e90f
intellij <patilprajwal8804@gmail.com>
2025-10-04 23:39:06 +0530

    done

---
+++ javaPrograms/Recursion/BasicRecursion/baseCase.java
@@ -0,0 +1,17 @@
+package Recursion.BasicRecursion;
+
+public class baseCase {
+    public static int factorial(int num) {
+
+        if(num==1){
+            return 1;
+        }
+        return num * factorial(num -1 );
+    }
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        System.out.println(factorial(num));
+    }
+}
