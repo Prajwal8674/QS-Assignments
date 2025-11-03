468eb7adb56096ffeaae375f5a8a3b1358f13e14
intellij <patilprajwal8804@gmail.com>
2025-11-04 00:12:33 +0530

    Commit

---
+++ Selenium/RelativeXPath/RelativeXPath.java
@@ -0,0 +1,20 @@
+package RelativeXPath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RelativeXPath {
+
+	public static void main(String[] args) {
+		 ChromeDriver driver = new ChromeDriver();
+		 driver.manage().window().maximize();
+		 
+		 driver.get("https://demowebshop.tricentis.com/");
+		 
+		 WebElement text = driver.findElement(By.xpath("(//strong)[5]"));
+
+		 System.out.println(text.getText());
+	}
+
+}
